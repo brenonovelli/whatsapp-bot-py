3a4c042f446a69f32c86f99ca707157dfc1ae23e
Breno Novelli <bnojpa@gmail.com>
2020-08-14 00:04:47 -0300

    First commit to Github

---
+++ README.md
@@ -0,0 +1,7 @@
+# WhatsApp bot
+
+Script para enviar mensagens para contatos específicos usando o WhatsApp Web.
+
+## Chromedriver
+
+Para o funcionamento do script, deve-se baixar e salvar na mesma pasta do projeto o Chromedriver da mesma versão do Chrome usado na sua máquina. https://chromedriver.chromium.org/downloads
---
+++ WGB.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+
+
+class WhatsappBot:
+    def __init__(self):
+        self.mensagem = "Fala meus camaradas. Estou testando um bot."
+        self.grupos_ou_pessoas = ["Rodrigo Gigante"]
+        options = webdriver.ChromeOptions()
+        options.add_argument('lang=pt-br')
+        self.driver = webdriver.Chrome(
+            executable_path=r'./chromedriver', options=options)
+
+    def EnviarMensagens(self):
+        self.driver.get('https://web.whatsapp.com/')
+        time.sleep(20)
+        for grupo_ou_pessoa in self.grupos_ou_pessoas:
+            campo_grupo = self.driver.find_element_by_xpath(
+                f"//span[@title='{grupo_ou_pessoa}']")
+            time.sleep(3)
+            campo_grupo.click()
+            chat_box = self.driver.find_element_by_class_name('_3uMse')
+            time.sleep(3)
+            chat_box.click()
+            chat_box.send_keys(self.mensagem)
+            botao_enviar = self.driver.find_element_by_xpath(
+                "//span[@data-icon='send']")
+            time.sleep(3)
+            botao_enviar.click()
+            time.sleep(5)
+
+
+bot = WhatsappBot()
+bot.EnviarMensagens()
